3529bacdbafe38323ce439fd08d74c07e830a56d
Ben Steward <ronyfan102@gmail.com>
2017-12-17 12:32:51 -0500

    touchup

--- certificates/js/main.js
+++ certificates/js/main.js
@@ -1,2 +1,6 @@
-$('.card-container').height($('img.card-side').height())
-$('.back').height($('img.card-side').height())
+function flipperFixup() {
+    $('.card-container').height($('img.card-side').height())
+    $('.back').height($('img.card-side').height())
+}
+flipperFixup()
+window.onresize = flipperFixup
